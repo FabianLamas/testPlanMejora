e9f69bf278f8c1ca51547e26b4c31c2b0c421942
Ian Clavero <ianzenit@gmail.com>
2019-11-11 14:54:30 -0300

    login finalizado

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormsModule } from '@angular/forms';
+import { MainService } from '../../servicios/main.service';
+import { Usuario } from '../../modelos/usuario';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  users: Usuario[] = [];
+  datosUsuario: any;
+
+  constructor( private usersService: MainService ) { }
 
   ngOnInit() {
+    this.users = this.usersService.getUsers();
   }
 
-  login() {
+  login(formLogin: NgForm) {
+
+    this.datosUsuario = formLogin.value;
+
+    for (let i = 0; i < this.users.length; i++) {
+
+      if (this.users[i].user == this.datosUsuario.user && this.users[i].pass == this.datosUsuario.pass) {
+        console.log('Aceptado');
+      } else {
+        console.log('Rechazado');
+      }
+
+    }
 
   }
 }
--- src/app/servicios/main.service.ts
+++ src/app/servicios/main.service.ts
@@ -6,7 +6,29 @@ import { Usuario } from '../modelos/usuario';
 })
 export class MainService {
 
- 
+  private users: Usuario[] = [
+    {
+      user: 'fabi',
+      pass: '123',
+      nombre: 'fabian',
+      apellido: 'lamas',
+      mail: 'fabian.lamas@kof.com.mx'
+    },
+    {
+      user: 'ian',
+      pass: '123',
+      nombre: 'ian',
+      apellido: 'clavero',
+      mail: 'ian.clavero@kof.com.mx'
+    }
+  ];
+
+  getUsers() {
+    return this.users;
+  }
 
   constructor() { }
-}
+
+  }
+
+
