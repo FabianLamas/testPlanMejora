6b78b6ca341735011c7e6cbd749917801bf69704
flamas <flamas@tgv.com.ar>
2019-11-13 16:28:28 -0300

    retocar tabla

--- src/app/componentes/responsables/responsables.component.ts
+++ src/app/componentes/responsables/responsables.component.ts
@@ -37,8 +37,8 @@ export class ResponsablesComponent implements OnInit {
                                 ];
   dataSource: MatTableDataSource<UserData>;
 
-  // @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  // @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   auditDocumentList: AuditDocument[];
   nonConformanceDocumentList: NonConformanceDocument[];
@@ -49,8 +49,8 @@ export class ResponsablesComponent implements OnInit {
 
   ngOnInit() {
     this.getAuditDocuments();
-    // this.dataSource.paginator = this.paginator;
-    // this.dataSource.sort = this.sort;
+    this.getNonConformanceDocuments();
+    
   }
 
   applyFilter(filterValue: string) {
@@ -59,21 +59,27 @@ export class ResponsablesComponent implements OnInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
-    this.getNonConformanceDocuments();
+    
   }
 
   generarLista() {
-
+// tslint:disable-next-line: prefer-for-of
+    if(this.auditDocumentList == undefined || this.nonConformanceDocumentList == undefined) return
     for (let i = 0; i < this.nonConformanceDocumentList.length; i++) {
+// tslint:disable-next-line: prefer-for-of
       for (let j = 0; j < this.auditDocumentList.length; j++) {
         if (this.nonConformanceDocumentList[i].auditDocument === this.auditDocumentList[j].id) {
-          var itemListaResp: ListaResponsables = new ListaResponsables( this.auditDocumentList[j].auditNumber, 'En proceso', this.nonConformanceDocumentList[i].number,'Fabian Lamas',this.nonConformanceDocumentList[i].creationStamp,5,this.nonConformanceDocumentList[i].sector,'Reclamos clientes',this.nonConformanceDocumentList[i].topic,'Naranja',this.nonConformanceDocumentList[i].size,this.nonConformanceDocumentList[i].responsible,this.nonConformanceDocumentList[i].answerType );
+// tslint:disable-next-line: max-line-length
+          var itemListaResp: ListaResponsables = new ListaResponsables( this.auditDocumentList[j].auditNumber, 'En proceso', this.nonConformanceDocumentList[i].number,'Fabian Lamas', this.nonConformanceDocumentList[i].creationStamp, 5 , this.nonConformanceDocumentList[i].sector, 'Reclamos clientes', this.nonConformanceDocumentList[i].topic,'Naranja', this.nonConformanceDocumentList[i].size,this.nonConformanceDocumentList[i].responsible, this.nonConformanceDocumentList[i].answerType );
         }
       }
       this.listaResponsables.push(itemListaResp);
     }
-    console.log(this.listaResponsables[1].number);
     this.dataSource = new MatTableDataSource(this.listaResponsables);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    console.log(this.dataSource);
+
   }
 
   getAuditDocuments() {
@@ -81,6 +87,7 @@ export class ResponsablesComponent implements OnInit {
       this.auditDocumentList = data;
       console.log('call API getAuditDocuments');
       console.log(this.auditDocumentList);
+      this.generarLista();
     }, error => {
       console.log('fallo el call de la API getAuditDocuments');
       console.log(error);
@@ -92,6 +99,7 @@ export class ResponsablesComponent implements OnInit {
       this.nonConformanceDocumentList = data;
       console.log('call API getNonConformanceDocuments');
       console.log(this.nonConformanceDocumentList);
+      this.generarLista();
     }, error => {
       console.log('fallo el call de la API getNonConformanceDocuments');
       console.log(error);
